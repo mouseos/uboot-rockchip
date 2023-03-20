1aa84eb61874aa13cd179b7ddc19d5f94f5dc3a6
yuu <46545607+mouseos@users.noreply.github.com>
2023-03-21 00:40:10 +0900

    add pyelftools

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Install toolchains
-sudo  apt install gcc-aarch64-linux-gnu
+sudo apt install gcc-aarch64-linux-gnu 
+pip3 install pyelftools
 # Create a directory to work in
 mkdir ~/evb_rk3399
 cd ~/evb_rk3399
