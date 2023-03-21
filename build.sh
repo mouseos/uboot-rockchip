d6099ce8c19714325eae86a7e9390ab7efc8ac68
yuu <46545607+mouseos@users.noreply.github.com>
2023-03-21 11:33:49 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Install toolchains
+apt search libusb-1.0
 sudo apt install device-tree-compiler gcc-arm-none-eabi curl python2 gcc-aarch64-linux-gnu libusb-1.0-0 libusb-1.0.0-dev
 #Change default python and pip version to python2
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python2 1
